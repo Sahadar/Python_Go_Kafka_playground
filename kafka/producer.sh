86aa12cf111d2a8d51d2b5989d1e3e75d69574b2
Wojciech Dlubacz <wdsahadar@gmail.com>
2019-02-17 14:52:06 +0100

    Delete logs

--- kafka/producer.sh
+++ kafka/producer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kafka_2.11-2.1.0/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+kafka_2.11-2.1.0/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic $1
