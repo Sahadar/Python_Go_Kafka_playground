822399661a389308bd69c4b6276b299624e063bd
Wojciech Dlubacz <wdsahadar@gmail.com>
2019-02-18 00:03:14 +0100

    Python pyramid playground

--- bin/start-webpage.sh
+++ bin/start-webpage.sh
@@ -7,9 +7,9 @@ export TIMEZONE="$(cat /etc/timezone)"
 SCRIPTPATH=$(dirname $0)
 
 docker-compose -f "$SCRIPTPATH/docker-compose.yml" stop playground-webpage
-docker-compose -f "$SCRIPTPATH/docker-compose.yml" -f "$SCRIPTPATH/docker-compose-webpage.override.yml" up --always-recreate-deps -d playground-webpage
+docker-compose -f "$SCRIPTPATH/docker-compose.yml" -f "$SCRIPTPATH/docker-compose-webpage.override.yml" up --remove-orphans --always-recreate-deps -d playground-webpage
 
 docker exec --user=playground -it playground-webpage bash -l
 
 docker-compose -f "$SCRIPTPATH/docker-compose.yml" stop playground-webpage
-docker-compose -f "$SCRIPTPATH/docker-compose.yml" up --always-recreate-deps -d playground-webpage
+docker-compose -f "$SCRIPTPATH/docker-compose.yml" up --remove-orphans --always-recreate-deps -d playground-webpage
--- bin/start.sh
+++ bin/start.sh
@@ -9,6 +9,6 @@ SCRIPTPATH=$(dirname $0)
 "$SCRIPTPATH/stop.sh"
 "$SCRIPTPATH/build.sh"
 
-docker-compose -f "$SCRIPTPATH/docker-compose.yml" up -d --force-recreate
+docker-compose -f "$SCRIPTPATH/docker-compose.yml" up --remove-orphans -d --force-recreate
 
 "$SCRIPTPATH/bash.sh"
