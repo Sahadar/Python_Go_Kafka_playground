3840920575577d5c4aba531a1ea7dadb6eb7f334
wd5170 <wojciech.dlubacz@hurra.com>
2019-02-18 13:01:05 +0100

    Run.sh for validator and kafka fixes

--- kafka/workspace/run.sh
+++ kafka/workspace/run.sh
@@ -2,5 +2,5 @@
 
 docker build -t playground-workspace-kafka .
 docker stop playground-kafka
-docker run -it -v "$PWD"."/../":/usr/src/playground -d --rm --name playground-kafka playground-workspace-kafka
+docker run -it -v "$PWD"."/../":/usr/src/playground -p 9092:9092 -p 2888:2888 -p 3888:3888 -d --rm --name playground-kafka playground-workspace-kafka
 ./bash.sh
--- url_validator/workspace/run.sh
+++ url_validator/workspace/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t playground-workspace-validator .
-docker run -it -v "$PWD"."/../":/usr/src/playground -v "$HOME/.aws/":/home/playground/.aws --rm --name playground-validator playground-workspace-validator bash
+docker run -it -v "$PWD"."/../":/usr/src/playground -v "$HOME/.aws/":/home/playground/.aws --link=playground-kafka:kafka --rm --name playground-validator playground-workspace-validator bash
