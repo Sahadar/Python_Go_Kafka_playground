8682e4c6f51465f113682df7c8867ff7d59cb10c
Wojciech Dlubacz <wdsahadar@gmail.com>
2019-02-20 00:28:56 +0100

    Run container

--- bin/run_container.sh
+++ bin/run_container.sh
@@ -4,8 +4,8 @@ export USERID="$(id -u)"
 export GROUPID="$(id -g)"
 export TIMEZONE="$(cat /etc/timezone)"
 
-SCRIPTPATH="$PWD/$(dirname $0)"
+SCRIPTPATH="$PWD/$(dirname $0)/../$1/workspace"
 
 docker build -t playground-workspace-kafka "$SCRIPTPATH/"
 docker stop playground-kafka
-docker run -it -v "$SCRIPTPATH/../":/usr/src/playground -p 9092:9092 -p 2888:2888 -p 3888:3888 --rm --name playground-kafka playground-workspace-kafka $@
+docker run -it -v "$SCRIPTPATH/../":/usr/src/playground -p 9092:9092 -p 2888:2888 -p 3888:3888 --rm --name playground-kafka playground-workspace-kafka ${@:2}
